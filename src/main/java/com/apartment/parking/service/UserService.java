e30134d25e373dbde5c70563273a66e1bbf98a4d
Dhanashree K <kdhanashree586@gmail.com>
2026-02-07 14:49:59 +0530

    Added backend module to SmartPark

--- src/main/java/com/apartment/parking/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.apartment.parking.service;
-import com.apartment.parking.dto.UserParkingResponse;
-import com.apartment.parking.entity.User;
-import com.apartment.parking.dto.UserParkingResponse;
-public interface UserService {
-    User register(User user);
-    User login(String identifier,String password);
-    UserParkingResponse getUserParkingDetails(Long userId);
-}
